436c44003447aa26f7f162dc9126bbaa86652f44
JaneVanilla <janevanilla92@gmail.com>
2023-03-05 08:19:33 +0100

    2.16 work on comments

--- project/src/pages/main-screen/main-screen.tsx
+++ project/src/pages/main-screen/main-screen.tsx
@@ -2,7 +2,7 @@ import Card from '../../components/card/card';
 type MainSreenProps = {
   offers: number;
 }
-export default function MainScreen(props: MainSreenProps) {
+export default function MainScreen({offers}: MainSreenProps) {
   return (
     <div className="page page--gray page--main">
       <div style={{display: 'none'}}>
@@ -92,7 +92,7 @@ export default function MainScreen(props: MainSreenProps) {
           <div className="cities__places-container container">
             <section className="cities__places places">
               <h2 className="visually-hidden">Places</h2>
-              <b className="places__found">{props.offers} places to stay in Amsterdam</b>
+              <b className="places__found">{offers} places to stay in Amsterdam</b>
               <form className="places__sorting" action="#" method="get">
                 <span className="places__sorting-caption">Sort by</span>
                 <span className="places__sorting-type" tabIndex={0}>
